a35663fa2255dcad9519234443938435b53869f9
Shubhendu Vaid <vaid.shubhendu@gmail.com>
2021-06-20 17:54:23 +0100

    game sound

--- init.js
+++ init.js
@@ -61,5 +61,5 @@ if (DEV_MODE) {
   loadSprite("pipe", "./images/pipe.png");
   loadSprite("boss", "./images/boss.png");
 
-  loadSound("gameAmbient", "./audios/startup.ogg");
+  loadSound("gameAmbient", "./audios/gameAmbient.ogg");
 }
